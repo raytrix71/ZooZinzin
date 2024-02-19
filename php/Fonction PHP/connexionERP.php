e2c6db5564d41ce7998af27c54898660eaf8faac
Dario <raytrix.minecraft@gmail.com>
2024-02-19 11:03:34 +0100

    modif structure site

---
+++ php/Fonction PHP/connexionERP.php
@@ -0,0 +1,16 @@
+<?php
+// Connect to the database
+$servername = "mysql_8.1.0_container";
+$username = "root";
+$password = "password";
+$dbname = "zoodb";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
