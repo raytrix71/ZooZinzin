0a952d94b441c2ab005e9d30b02bb6885e8cd7f5
Dario <raytrix.minecraft@gmail.com>
2024-03-01 15:30:07 +0100

    session essaie

--- php/Fonction_PHP/connexionUtilisateurERP.php
+++ php/Fonction_PHP/connexionUtilisateurERP.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+
 include 'connexionDB.php';
 function connERP(){
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -13,12 +13,47 @@ function connERP(){
                 'mail' => $mail,
                 'mdp' => $mdp
             ));
-            $resultat = $connexion->fetch();
-            $_SESSION['id'] = $resultat['IDEmploye'];
-            $_SESSION['prenom'] = $resultat['PrenomEmploye'];
-            $_SESSION['nom'] = $resultat['NomEmploye'];
-            header('Location: ../ERP/Dashboard/dashboard.php');
-           
+            $resultat = $connexion->fetchAll();
+            var_dump($resultat);
+        echo "\n Bienvenue " . $resultat[0]['PrenomEmploye'];
+        echo "\n Bienvenue " . $resultat[0]['NomEmploye'];
+        echo "\n Bienvenue " . $resultat[0]['IDEmploye'];
+            // Stockez les informations de l'employé dans la session
+            $_SESSION['id'] = $resultat[0]['IDEmploye'];
+            $_SESSION['prenom'] = $resultat[0]['PrenomEmploye'];
+            $_SESSION['nom'] = $resultat[0]['NomEmploye'];
+            header("Location: ../ERP/Dashboard/dashboard.php");
+            
+        } else {
+            echo "Les champs mail et mdp doivent être renseignés.";
+        }
+    } else {
+        echo "Aucune donnée soumise par le formulaire.";
+    }
+}
+connERP(); 
+
+/*session_start();
+include 'connexionDB.php';
+function connERP(){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST['mail']) && isset($_POST['mdp'])) {
+            $mail = $_POST['mail'];
+            $mdp = $_POST['mdp'];
+            $bdd=connexionDB();
+            $reqsql = 'SELECT IDEmploye,PrenomEmploye,NomEmploye FROM EMPLOYE WHERE MailEmploye = :mail AND MDPEmploye = :mdp';
+            $connexion = $bdd->prepare($reqsql);
+            $connexion->execute(array(
+                'mail' => $mail,
+                'mdp' => $mdp
+            ));
+            $resultat = $connexion->fetchAll();
+
+            // Stockez les informations de l'employé dans la session
+            $_SESSION['id'] = $resultat[0]['IDEmploye'];
+            $_SESSION['prenom'] = $resultat[0]['PrenomEmploye'];
+            $_SESSION['nom'] = $resultat[0]['NomEmploye'];
+            header("Location: ../ERP/Dashboard/dashboard.php");
         } else {
             echo "Les champs mail et mdp doivent être renseignés.";
         }
@@ -27,3 +62,5 @@ function connERP(){
     }
 }
 connERP();
+*/
+?> 
