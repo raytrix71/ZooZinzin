59c949195404dc2fcfc4cf9f8736ba3600f2a186
Dario <raytrix.minecraft@gmail.com>
2024-03-04 11:14:08 +0100

    connexion et style de la navbar

--- php/ERP/NavBar/navbar.php
+++ php/ERP/NavBar/navbar.php
@@ -6,9 +6,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>MenuNav</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/fonts/material-icons.min.css">
-    <link rel="stylesheet" href="assets/css/Navbar-Right-Links-icons.css">
+    <link rel="stylesheet" href="../NavBar/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/NavBar/fonts/material-icons.min.css">
+    <link rel="stylesheet" href="assets/NavBar/css/Navbar-Right-Links-icons.css">
 </head>
 
 <body>
@@ -134,7 +134,7 @@
             </div>
         </div>
     </div>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../NavBar/assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
