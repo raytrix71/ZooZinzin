79ace7c5e0d6ca428e2f28bfede3f543c7a761f5
Dario <raytrix.minecraft@gmail.com>
2024-03-05 16:18:05 +0100

    changement Employe

---
+++ php/Fonction_PHP/connERP.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+include '../Model/Employe.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST['mail']) && isset($_POST['mdp'])) {
+            $mail = $_POST['mail'];
+            $mdp = $_POST['mdp'];
+            $listeEmploye = Employe::getListeEmploye();
+            
+        
+            foreach($listeEmploye as $employe){
+                if($employe->get_mailEmploye() == $mail && password_verify($mdp, $employe->get_mdpEmploye())){
+                    $_SESSION['id'] = $employe->get_idEmploye();
+                    $_SESSION['prenom'] = $employe->get_prenomEmploye();
+                    $_SESSION['nom'] = $employe->get_nomEmploye();
+                    header("Location: ../ERP/Dashboard/dashboard.php");
+                }
+            
+            } ;
+        }
+        else{
+            echo "Les champs mail et mdp doivent être renseignés.";
+        }
+    } 
+    
+    else {
+        echo "Aucune donnée soumise par le formulaire.";
+    }
+
+
+
+ 
+?>
+
--- php/Model/Employe.php
+++ php/Model/Employe.php
@@ -1,18 +1,19 @@
+
 <?php
 include ('../Fonction_PHP/connexionDB.php');
 class Employe{
 
     static $listeEmploye=array();
-    private $idEmploye;
-    private $nomEmploye;
-    private $prenomEmploye;
-    private $mailEmploye;
-    private $mdpEmploye;
-    private $roleEmploye;
-    private $adresse;
-    private $CP;
-    private $tel;
-    private $idzone;
+    var $idEmploye;
+    var $nomEmploye;
+    var $prenomEmploye;
+    var $mailEmploye;
+    var $mdpEmploye;
+    var $roleEmploye;
+    var $adresse;
+    var $CP;
+    var $tel;
+    var $idzone;
 
     function __construct($idEmploye,$prenomEmploye,$nomEmploye,$adresse,$CP,$mailEmploye,$mdpEmploye,$tel,$idzone,$roleEmploye){
         $this->idEmploye = $idEmploye;
@@ -25,8 +26,26 @@ class Employe{
         $this->CP = $CP;
         $this->tel = $tel;
         $this->idzone = $idzone;
-
+        
     }
+
+   /* function __construct($idEmploye,$prenomEmploye,$nomEmploye,$adresse,$CP,$mailEmploye,$mdpEmploye,$tel,$roleEmploye){
+        $this->idEmploye = $idEmploye;
+        $this->nomEmploye = $nomEmploye;
+        $this->prenomEmploye = $prenomEmploye;
+        $this->mailEmploye = $mailEmploye;
+        $this->mdpEmploye = $mdpEmploye;
+        $this->roleEmploye = $roleEmploye;
+        $this->adresse = $adresse;
+        $this->CP = $CP;
+        $this->tel = $tel;
+        $bdd = connexionDB();
+        $sql = "INSERT INTO EMPLOYE (IDEmploye, PrenomEmploye, NomEmploye, AdresseEmploye, CPEmploye, MailEmploye, MDPEmploye, TelEmploye, Role) VALUES ('$idEmploye', '$prenomEmploye', '$nomEmploye', '$adresse', '$CP', '$mailEmploye', '$mdpEmploye', '$tel', '$roleEmploye')";
+        $bdd->exec($sql);
+    } */
+    
+        
+    
 
 
     static function getListeEmploye(){
@@ -44,28 +63,103 @@ class Employe{
         $connexion = $bdd->query($sql);
         $resultat = $connexion->fetchAll();
         foreach($resultat as $row){
+            if($row['IDZone'] == NULL){
+                $row['IDZone'] = 0;
+            }
             $employe = new Employe($row['IDEmploye'],$row['PrenomEmploye'],$row['NomEmploye'],$row['AdresseEmploye'],$row['CPEmploye'],$row['MailEmploye'],$row['MDPEmploye'],$row['TelEmploye'],$row['IDZone'],$row['Role']);
             array_push(self::$listeEmploye,$employe);
         }
 
     }
 
-    function getEmailEmploye(){
-        return $this->mailEmploye;
+    function get_idEmploye(){
+        return $this->idEmploye;
     }
 
-    function getMdpEmploye(){
-        return $this->mdpEmploye;
+    function get_nomEmploye(){
+        return $this->nomEmploye;
     }
 
-    function getPrenomEmploye(){
+    function get_prenomEmploye(){
         return $this->prenomEmploye;
     }
 
+    function get_mailEmploye(){
+        return $this->mailEmploye;
+    }
 
+    function get_mdpEmploye(){
+        return $this->mdpEmploye;
+    }
+
+    function get_roleEmploye(){
+        return $this->roleEmploye;
+    }
+
+    function get_adresseEmploye(){
+        return $this->adresse;
+    }
+
+    function get_CPEmploye(){
+        return $this->CP;
+    }
+
+    function get_telEmploye(){
+        return $this->tel;
+    }
+
+    function get_idzone(){
+        return $this->idzone;
+    }
+
+    function set_idEmploye($idEmploye){
+        $this->idEmploye = $idEmploye;
+    }
+
+    function set_nomEmploye($nomEmploye){
+        $this->nomEmploye = $nomEmploye;
+    }
+
+    function set_prenomEmploye($prenomEmploye){
+        $this->prenomEmploye = $prenomEmploye;
+    }
+
+    function set_mailEmploye($mailEmploye){
+        $this->mailEmploye = $mailEmploye;
+    }
+
+    function set_mdpEmploye($mdpEmploye){
+        $this->mdpEmploye = $mdpEmploye;
+    }
+
+    function set_roleEmploye($roleEmploye){
+        $this->roleEmploye = $roleEmploye;
+    }
+
+    function set_adresseEmploye($adresse){
+        $this->adresse = $adresse;
+    }
+
+    function set_CPEmploye($CP){
+        $this->CP = $CP;
+    }
+
+    function set_telEmploye($tel){
+        $this->tel = $tel;
+    }
+
+    function set_idzone($idzone){
+        $this->idzone = $idzone;
+    }
+
+    function __toString(){
+        return $this->idEmploye . " " . $this->nomEmploye . " " . $this->prenomEmploye . " " . $this->mailEmploye . " " . $this->mdpEmploye . " " . $this->roleEmploye . " " . $this->adresse . " " . $this->CP . " " . $this->tel . " " . $this->idzone;
+    }
 
 
 
 }
+
+
 
 ?>
