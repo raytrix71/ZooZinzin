2562cdc551609979fda949201255dbc83dfb8af0
Dario <raytrix.minecraft@gmail.com>
2024-03-17 10:27:06 +0100

    QR code

--- php/ERP/GestionBillets/ScanEntree/scanentree.php
+++ php/ERP/GestionBillets/ScanEntree/scanentree.php
@@ -8,25 +8,24 @@
 <div id="qr-reader" style="width:500px"></div>
 <div id="qr-reader-results"></div>
 
-    <script>
-        var resultContainer = document.getElementById('qr-reader-results');
-        var lastResult, countResults = 0;
+<script>
+    var resultContainer = document.getElementById('qr-reader-results');
+    var lastResult, countResults = 0;
 
-        function onScanSuccess(decodedText, decodedResult) {
-            if (decodedText !== lastResult) {
-                ++countResults;
-                lastResult = decodedText;
-                // Handle on success condition with the decoded message.
-                console.log(`Scan result ${decodedText}`, decodedResult);
-                // Rediriger vers une autre page
-                window.location.href = "/ERP/GestionBillets/ResultatsScan.php/resultatsscanEntree.php?idresa=" + decodedText;
-            }
+    function onScanSuccess(decodedText, decodedResult) {
+        if (decodedText !== lastResult) {
+            ++countResults;
+            lastResult = decodedText;
+            // Handle on success condition with the decoded message.
+            console.log(`Scan result ${decodedText}`, decodedResult);
+            // Rediriger vers une autre page
+            window.location.href = "/ERP/GestionBillets/ResultatsScan.php/resultatsscanEntree.php?idresa=" + decodedText;
         }
+    }
 
-
-var html5QrcodeScanner = new Html5QrcodeScanner(
-    "qr-reader", { fps: 10, qrbox: 250 });
-html5QrcodeScanner.render(onScanSuccess);
-    </script>
+    var html5QrcodeScanner = new Html5QrcodeScanner(
+        "qr-reader", { fps: 10, qrbox: 250, preferredCameraDevice: 'environment' });
+    html5QrcodeScanner.render(onScanSuccess);
+</script>
 </body>
 </html>
---
+++ php/SIteWebZoo/Billets/Billet.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+    <script src="qrcode.min.js"></script>
+</head>
+<body>
+    <h1>Welcome to My Website</h1>
+    <div id="qrcode"></div>
+<script type="text/javascript">
+new QRCode(document.getElementById("qrcode"), "1");
+var qrcode = new QRCode("test", {
+    text: "http://jindo.dev.naver.com/collie",
+    width: 128,
+    height: 128,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});
+</script>
+    <?php
+    // Your PHP code goes here
+    ?>
+    
+</body>
+</html>
