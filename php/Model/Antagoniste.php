10b3367ecc869c1d1077bfc287c9f9c3036807f4
Dario <raytrix.minecraft@gmail.com>
2024-03-19 14:12:56 +0100

    ouiouibaguette

--- php/Model/Antagoniste.php
+++ php/Model/Antagoniste.php
@@ -47,7 +47,7 @@ class Antagoniste
 
     public static function fetchAntagonisteFromDB(){
         $conn= DB::connexionDB();
-        $sql = "SELECT * FROM antagoniste";
+        $sql = "SELECT * FROM ANTAGONISTE";
         $result = $conn->query($sql);
         $listAntagoniste = array();
         $listAntagoniste= $result->fetchAll();
@@ -60,11 +60,22 @@ class Antagoniste
         return $final;
     }
 
-   
-
-
+public function ajoutAntagonisteToDB(){
+    $conn= DB::connexionDB();
+    $sql = "INSERT INTO ANTAGONISTE (NomEspecePredateur, NomEspeceProie) VALUES (:NomEspecePredateur, :NomEspeceProie)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':NomEspecePredateur', $this->NomEspecePredateur);
+    $stmt->bindParam(':NomEspeceProie', $this->NomEspeceProie);
+    $stmt->execute();
 }
 
+public function suppressionAntagonisteFromDB(){
+    $conn= DB::connexionDB();
+    $sql = "DELETE FROM ANTAGONISTE WHERE IDAntagoniste = :IDAntagoniste";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':IDAntagoniste', $this->IDAntagoniste);
+    $stmt->execute();
 
-
+}///fin class
+}
 ?>
