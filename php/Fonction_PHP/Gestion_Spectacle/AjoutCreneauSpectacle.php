6b5b40d4e0d659324edd37ac34a29170acf06588
bazi9898 <edsko697@gmail.com>
2024-03-25 15:56:32 +0100

    spec

--- php/Fonction_PHP/Gestion_Spectacle/AjoutCreneauSpectacle.php
+++ php/Fonction_PHP/Gestion_Spectacle/AjoutCreneauSpectacle.php
@@ -1,27 +1,16 @@
 <?php 
+
 include_once '/var/www/html/Model/DB.php';
 include_once '/var/www/html/Model/Spectacle.php';
 include '/var/www/html/Model/TypeSpectacle.php';
 
-var_dump($IDTypeSpectacle);
-
-
-$IDTypeSpectacle = $_POST['IDTypeSpectacle'];
+$idTypeSpectacle = $_POST['IDTypeSpectacle'];
 $dateSpectacle = $_POST['DateSpectacle'];
 $heureSpectacle = $_POST['HeureSpectacle'];
 
-// Récupération de l'instance de TypeSpectacle
-$typeSpectacle = TypeSpectacle::fetchByID($IDTypeSpectacle);
 
-if ($typeSpectacle === null) {
-    echo "Type de spectacle introuvable";
-    exit();
-}
 
-// Création de l'instance de Spectacle
-$creneau = new Spectacle(null, $typeSpectacle, $dateSpectacle, $heureSpectacle);
-
+$creneau = new Spectacle(null, $idTypeSpectacle,  $dateSpectacle, $heureSpectacle);
 $creneau->saveToDatabase();
 
-
-?>
+header('Location: /ERP/GestionBillets/Spectacle/CreneauSpectacle.php?IDTypeSpectacle='.$idTypeSpectacle);
