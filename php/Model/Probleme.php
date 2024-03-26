ba81020b77a941b0c1a8dcf583df4a99c711c37b
Dario <raytrix.minecraft@gmail.com>
2024-03-26 17:16:19 +0100

    Probleme

--- php/Model/Probleme.php
+++ php/Model/Probleme.php
@@ -7,9 +7,9 @@ class Probleme {
     private $DescriptionPB;
     private $DatePB;
     private $SoinsRealiseAvantIntervention;
-    private $StatutProblene;
+    private $StatutProbleme;
 
-    public function __construct($IDProbleme, $IDAnimal, $IDGroupe, $IDEmploye, $DescriptionPB, $DatePB, $SoinsRealiseAvantIntervention, $StatutProblene) {
+    public function __construct($IDProbleme, $IDAnimal, $IDGroupe, $IDEmploye, $DescriptionPB, $DatePB, $SoinsRealiseAvantIntervention, $StatutProbleme) {
         $this->IDProbleme = $IDProbleme;
         $this->IDAnimal = $IDAnimal;
         $this->IDGroupe = $IDGroupe;
@@ -17,7 +17,7 @@ class Probleme {
         $this->DescriptionPB = $DescriptionPB;
         $this->DatePB = $DatePB;
         $this->SoinsRealiseAvantIntervention = $SoinsRealiseAvantIntervention;
-        $this->StatutProblene = $StatutProblene;
+        $this->StatutProbleme = $StatutProbleme;
     }
 
     public function getIDProbleme() {
@@ -48,8 +48,66 @@ class Probleme {
         return $this->SoinsRealiseAvantIntervention;
     }
 
-    public function getStatutProblene() {
-        return $this->StatutProblene;
+    public function getStatutProbleme() {
+        return $this->StatutProbleme;
     }
 
+    public function setIDProbleme($IDProbleme) {
+        $this->IDProbleme = $IDProbleme;
+    }
+
+    public function setIDAnimal($IDAnimal) {
+        $this->IDAnimal = $IDAnimal;
+    }
+
+    public function setIDGroupe($IDGroupe) {
+        $this->IDGroupe = $IDGroupe;
+    }
+
+    public function setIDEmploye($IDEmploye) {
+        $this->IDEmploye = $IDEmploye;
+    }
+
+    public function setDescriptionPB($DescriptionPB) {
+        $this->DescriptionPB = $DescriptionPB;
+    }
+
+    public function setDatePB($DatePB) {
+        $this->DatePB = $DatePB;
+    }
+
+    public function setSoinsRealiseAvantIntervention($SoinsRealiseAvantIntervention) {
+        $this->SoinsRealiseAvantIntervention = $SoinsRealiseAvantIntervention;
+    }
+
+    public function setStatutProblene($StatutProbleme) {
+        $this->StatutProbleme = $StatutProbleme;
+    }
+
+    public function ajoutDBAnimal(){
+        $conn = DB::connexionDB();
+        $sql = "INSERT INTO PROBLEME (IDAnimal, IDEmploye, DescriptionPB, DatePB, SoinsRealiseAvantIntervention, StatutProbleme) VALUES (:IDAnimal, :IDEmploye, :DescriptionPB, :DatePB, :SoinsRealiseAvantIntervention, :StatutProbleme)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':IDAnimal', $this->IDAnimal);
+        $stmt->bindParam(':IDEmploye', $this->IDEmploye);
+        $stmt->bindParam(':DescriptionPB', $this->DescriptionPB);
+        $stmt->bindParam(':DatePB', $this->DatePB);
+        $stmt->bindParam(':SoinsRealiseAvantIntervention', $this->SoinsRealiseAvantIntervention);
+        $stmt->bindParam(':StatutProbleme', $this->StatutProbleme);
+        $stmt->execute();
+    }
+
+    public function ajoutDBGroupe(){
+        $conn = DB::connexionDB();
+        $sql = "INSERT INTO PROBLEME (IDGroupe, IDEmploye, DescriptionPB, DatePB, SoinsRealiseAvantIntervention, StatutProbleme) VALUES (:IDGroupe, :IDEmploye, :DescriptionPB, :DatePB, :SoinsRealiseAvantIntervention, :StatutProbleme)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':IDGroupe', $this->IDGroupe);
+        $stmt->bindParam(':IDEmploye', $this->IDEmploye);
+        $stmt->bindParam(':DescriptionPB', $this->DescriptionPB);
+        $stmt->bindParam(':DatePB', $this->DatePB);
+        $stmt->bindParam(':SoinsRealiseAvantIntervention', $this->SoinsRealiseAvantIntervention);
+        $stmt->bindParam(':StatutProbleme', $this->StatutProbleme);
+        $stmt->execute();
+    }
+    
 }
