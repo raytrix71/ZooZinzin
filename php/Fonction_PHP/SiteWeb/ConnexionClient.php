6ae5ca0756334cf1655924dcf0dab993e7bbde77
Dario <raytrix.minecraft@gmail.com>
2024-03-27 21:25:03 +0100

    uiui

--- php/Fonction_PHP/SiteWeb/ConnexionClient.php
+++ php/Fonction_PHP/SiteWeb/ConnexionClient.php
@@ -5,7 +5,7 @@ $listeclient=Client::getListeClient();
 foreach($listeclient as $client){
     if($client->getEmailClient()==$mail && password_verify($mdp,$client->getMdpClient())){
         session_start();
-        $_SESSION['connecte']=true;
+        $_SESSION['connecte']="loggedIn";
         $_SESSION['prenomclient']=$client->getPrenomClient();
         $_SESSION['nomclient']=$client->getNomClient();
         $_SESSION['idclient']=$client->getIdClient();
