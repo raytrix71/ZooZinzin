82d690debcbae45485266456354c96619d2625fd
bazi9898 <edsko697@gmail.com>
2024-03-27 22:47:13 +0100

    KOu

--- php/Fonction_PHP/SiteWeb/ValiderPanier.php
+++ php/Fonction_PHP/SiteWeb/ValiderPanier.php
@@ -5,7 +5,7 @@ $i=$_POST['i'];
 $j=$_POST['j'];
 $k=$_POST['k'];
 $dateResaBillet=$_POST['date'];
-$idClient=$_Session['idClient'];
+$idClient=$_SESSION['idclient'];
 $dateAjd = date('Y-m-d');
 $newresa=new Reservation(null,$idClient,null,$dateAjd);
 $newresa->saveReservation();
